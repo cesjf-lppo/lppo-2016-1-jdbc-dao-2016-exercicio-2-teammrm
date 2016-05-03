368a65a028c08910fd86b5e094cc1444710e330e
aluno <aluno@L4MAQ18.labcesac.net>
2016-05-02 22:28:37 -0300

    criar conexao e dao

---
+++ src/java/br/cesjf/lppo/AtividadeDAO.java
@@ -0,0 +1,51 @@
+package br.cesjf.lppo;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class AtividadeDAO {
+
+    List<Atividade> listaTodos() throws Exception {
+        List<Atividade> todos = new ArrayList<>();
+        try {
+            Connection conexao = ConexaoJDBC.getInstance();
+            Statement operacao = conexao.createStatement();
+            ResultSet resultado = operacao.executeQuery("SELECT * FROM estabelecimento");
+            while (resultado.next()) {
+                Atividade ativ = new Atividade();
+                ativ.setId(resultado.getLong("id"));
+                ativ.setDescricao(resultado.getString("Descricao"));
+                ativ.setFuncionario(resultado.getString("Funcionario"));
+                ativ.setHoras(resultado.getInt("Horas"));
+                ativ.setTipo(resultado.getString("Tipo"));
+                ativ.setId(resultado.getLong("id"));
+                todos.add(ativ);
+            }
+
+        } catch (SQLException ex) {
+            Logger.getLogger(AtividadeDAO.class.getName()).log(Level.SEVERE, null, ex);
+            throw new Exception(ex);
+        }
+
+        return todos;
+    }
+
+    void criar(Atividade novaativ) throws Exception {
+        try {
+            Connection conexao = ConexaoJDBC.getInstance();
+            Statement operacao = conexao.createStatement();
+            operacao.executeUpdate(String.format("INSERT INTO atividade(Descricao, Funcionario, Horas, Tipo) VALUES('%s','%s','%s','%s','%s')", novaativ.getDescricao(), novaativ.getFuncionario(),novaativ.getHoras(),novaativ.getTipo()));
+
+        } catch (SQLException ex) {
+            Logger.getLogger(AtividadeDAO.class.getName()).log(Level.SEVERE, null, ex);
+            throw new Exception(ex);
+        }
+    }
+}
---
+++ src/java/br/cesjf/lppo/ConexaoJDBC.java
@@ -0,0 +1,21 @@
+
+package br.cesjf.lppo;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+public class ConexaoJDBC {
+    private static Connection instancia = null;
+    
+    private ConexaoJDBC(){};
+    
+    public static Connection getInstance() throws SQLException{
+        if(instancia == null){
+            instancia = DriverManager.getConnection("jdbc:derby://localhost:1527/lppo-2016-1", "usuario", "1234");
+        }
+        return instancia;
+    }
+    
+}
