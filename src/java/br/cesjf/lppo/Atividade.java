4ae24179c92d8bd0a87e008807726236bc0ab594
aluno <aluno@L4MAQ17.labcesac.net>
2016-05-02 21:46:16 -0300

    Getter e Setter Classe Atividade

--- src/java/br/cesjf/lppo/Atividade.java
+++ src/java/br/cesjf/lppo/Atividade.java
@@ -6,4 +6,48 @@ public class Atividade {
     private String descricao;
     private String tipo;
     private Integer horas;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFuncionario() {
+        return funcionario; 
+    }
+
+    public void setFuncionario(String funcionario) {
+        this.funcionario = funcionario;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public Integer getHoras() {
+        return horas;
+    }
+
+    public void setHoras(Integer horas) {
+        this.horas = horas;
+    }
+    
+    
+    
 }
+
